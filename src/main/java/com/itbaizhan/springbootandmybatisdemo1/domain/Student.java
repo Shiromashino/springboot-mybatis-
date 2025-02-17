e23d36ee95ce8d622f51c47cf3d1cb7ff7e1db54
结城明日奈 <2528216450@qq.com>
2025-02-17 17:23:08 +0800

    添加功能

--- src/main/java/com/itbaizhan/springbootandmybatisdemo1/domain/Student.java
+++ src/main/java/com/itbaizhan/springbootandmybatisdemo1/domain/Student.java
@@ -1,15 +1,26 @@
 package com.itbaizhan.springbootandmybatisdemo1.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Student {
-    private int id;
+    private Integer id;
     private String name;
     private String sex;
     private String address;
 }
+
+//实体类转表名：
+//Student → student
+//UserInfo → user_info
+//OrderDetail → order_detail
+//这叫做"驼峰转下划线"命名规则：
+//
+//大写字母会变成小写，并在前面加上下划线
+//第一个大写字母只会变成小写，前面不加下划线
--- src/test/java/com/itbaizhan/springbootandmybatisdemo1/mapper/studentMapperTest.java
+++ src/test/java/com/itbaizhan/springbootandmybatisdemo1/mapper/studentMapperTest.java
@@ -2,17 +2,26 @@ package com.itbaizhan.springbootandmybatisdemo1.mapper;
 
 import com.itbaizhan.springbootandmybatisdemo1.domain.Student;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class StudentMapperTest {
+public class studentMapperTest {
     @Autowired
     private StudentMapper studentMapper;
 
     @Test
-    public void test(){
+    public void testFind(){
         Student student = studentMapper.selectById(1);
         System.out.println(student);
+    }
+
+    @Test
+    public void testAdd(){
+        Student student = Student.builder().id(5).name("宫子").sex("女").address("fox").build();
+
+        studentMapper.insert(student);
+        student.getId();
     }
 }
