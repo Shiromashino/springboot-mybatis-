7e8b87b4b02ac9256ae4fd0deae398c1d224cec8
结城明日奈 <2528216450@qq.com>
2025-02-21 20:21:26 +0800

    mybatisx插件自动生成代码

--- src/main/java/com/itbaizhan/springbootandmybatisdemo1/domain/Login.java
+++ src/main/java/com/itbaizhan/springbootandmybatisdemo1/domain/Login.java
@@ -1,35 +1,79 @@
 package com.itbaizhan.springbootandmybatisdemo1.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 /**
- * <p>
  * 
- * </p>
- *
- * @author pxb
- * @since 02-21
+ * @TableName login
  */
-@Getter
-@Setter
-@TableName("login")
-public class Login implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @TableId(value = "id", type = IdType.AUTO)
+@TableName(value ="login")
+@Data
+public class Login {
+    /**
+     * 
+     */
+    @TableId(type = IdType.AUTO)
     private Integer id;
 
+    /**
+     * 
+     */
     private String name;
 
+    /**
+     * 
+     */
     private String password;
 
+    /**
+     * 
+     */
     private Boolean role;
 
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Login other = (Login) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
+            && (this.getRole() == null ? other.getRole() == null : this.getRole().equals(other.getRole()));
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        result = prime * result + ((getRole() == null) ? 0 : getRole().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", password=").append(password);
+        sb.append(", role=").append(role);
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- src/main/java/com/itbaizhan/springbootandmybatisdemo1/service/impl/LoginServiceImpl.java
+++ src/main/java/com/itbaizhan/springbootandmybatisdemo1/service/impl/LoginServiceImpl.java
@@ -1,20 +1,22 @@
 package com.itbaizhan.springbootandmybatisdemo1.service.impl;
 
-import com.itbaizhan.springbootandmybatisdemo1.domain.Login;
-import com.itbaizhan.springbootandmybatisdemo1.mapper.LoginMapper;
-import com.itbaizhan.springbootandmybatisdemo1.service.ILoginService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itbaizhan.springbootandmybatisdemo1.domain.Login;
+import com.itbaizhan.springbootandmybatisdemo1.service.LoginService;
+import com.itbaizhan.springbootandmybatisdemo1.mapper.LoginMapper;
 import org.springframework.stereotype.Service;
 
 /**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author pxb
- * @since 02-21
- */
+* @author 86177
+* @description 针对表【login】的数据库操作Service实现
+* @createDate 2025-02-21 20:16:12
+*/
 @Service
-public class LoginServiceImpl extends ServiceImpl<LoginMapper, Login> implements ILoginService {
+public class LoginServiceImpl extends ServiceImpl<LoginMapper, Login>
+    implements LoginService{
 
 }
+
+
+
+
