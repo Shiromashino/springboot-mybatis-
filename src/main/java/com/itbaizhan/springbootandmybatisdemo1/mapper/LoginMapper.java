019faea8df7ee088d5f4e63b45661abd28f535ae
结城明日奈 <2528216450@qq.com>
2025-02-21 20:23:00 +0800

    mybatisx插件自动生成代码

---
+++ src/main/java/com/itbaizhan/springbootandmybatisdemo1/mapper/LoginMapper.java
@@ -0,0 +1,18 @@
+package com.itbaizhan.springbootandmybatisdemo1.mapper;
+
+import com.itbaizhan.springbootandmybatisdemo1.domain.Login;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86177
+* @description 针对表【login】的数据库操作Mapper
+* @createDate 2025-02-21 20:16:12
+* @Entity com.itbaizhan.springbootandmybatisdemo1.domain.Login
+*/
+public interface LoginMapper extends BaseMapper<Login> {
+
+}
+
+
+
+
---
+++ src/main/java/com/itbaizhan/springbootandmybatisdemo1/service/LoginService.java
@@ -0,0 +1,13 @@
+package com.itbaizhan.springbootandmybatisdemo1.service;
+
+import com.itbaizhan.springbootandmybatisdemo1.domain.Login;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 86177
+* @description 针对表【login】的数据库操作Service
+* @createDate 2025-02-21 20:16:12
+*/
+public interface LoginService extends IService<Login> {
+
+}
